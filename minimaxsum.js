e66e2db1a88e7c878d1106e5666f0a2d89d41db1
Slicktone <tclaytony23@gmail.com>
2022-03-30 15:12:50 -0700

    cleaning up :-)

--- minimaxsum.js
+++ minimaxsum.js
@@ -1,30 +1,3 @@
-'use strict';
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-/*
- * Complete the 'miniMaxSum' function below.
- *
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
 
 function miniMaxSum(arr) {
     // sort array
@@ -40,15 +13,4 @@ function miniMaxSum(arr) {
 
     //print max and min
     return console.log(maxSum, minSum);
-}
-
-const arr = [5, 4, 3, 2, 1];
-miniMaxSum(arr)
-
-
-function main() {
-
-    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
-
-    miniMaxSum(arr);
 }
